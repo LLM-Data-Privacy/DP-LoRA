3549882064bde79a1aad6fe62c3941935719eb4f
icedfool <50580723+icedfool@users.noreply.github.com>
2024-02-06 17:02:31 -0500

    first testing file added

---
+++ test.py
@@ -0,0 +1,55 @@
+import os
+import time
+
+import fedml
+
+if __name__ == "__main__":
+    print("Hi everyone, I am an launch job.")
+
+    print(f"current config is {fedml.get_env_version()}")
+
+    run_id = os.getenv('FEDML_CURRENT_RUN_ID', 0)
+    edge_id = os.getenv('FEDML_CURRENT_EDGE_ID', 0)
+    local_on_prem_host = fedml.get_local_on_premise_platform_host()
+    local_on_prem_port = fedml.get_local_on_premise_platform_port()
+    print(f"local_on_prem_host {local_on_prem_host}, local_on_prem_port {local_on_prem_port}")
+
+    acc = 0.1
+    loss = 2.0
+    for iter_count in range(10):
+        acc += 0.01
+        loss -= 0.02
+        #fedml.log_endpoint({"acc": acc, "loss": loss})
+        time.sleep(0.1)
+
+    artifact = fedml.mlops.Artifact(name=f"general-file@{run_id}-{edge_id}", type=fedml.mlops.ARTIFACT_TYPE_NAME_GENERAL)
+    artifact.add_file("./requirements.txt")
+    artifact.add_dir("./config")
+    fedml.mlops.log_artifact(artifact)
+
+    fedml.mlops.log_model(f"model-file@{run_id}-{edge_id}", "./requirements.txt")
+
+    artifact = fedml.mlops.Artifact(name=f"log-file@{run_id}-{edge_id}", type=fedml.mlops.ARTIFACT_TYPE_NAME_LOG)
+    artifact.add_file("./requirements.txt")
+    artifact.add_dir("./config")
+    fedml.mlops.log_artifact(artifact)
+
+    artifact = fedml.mlops.Artifact(name=f"source-file@{run_id}-{edge_id}", type=fedml.mlops.ARTIFACT_TYPE_NAME_SOURCE)
+    artifact.add_file("./requirements.txt")
+    artifact.add_dir("./config")
+    fedml.mlops.log_artifact(artifact)
+
+    artifact = fedml.mlops.Artifact(name=f"dataset-file@{run_id}-{edge_id}", type=fedml.mlops.ARTIFACT_TYPE_NAME_DATASET)
+    artifact.add_file("./requirements.txt")
+    artifact.add_dir("./config")
+    fedml.mlops.log_artifact(artifact)
+
+    acc = 0.1
+    loss = 2.0
+    for iter_count in range(10):
+        acc += 0.01
+        loss -= 0.02
+        fedml.mlops.log_metric({"acc": acc, "loss": loss})
+        time.sleep(2)
+
+    time.sleep(10)
