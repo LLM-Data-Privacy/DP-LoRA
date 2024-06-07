95bb18e8385dc1cf190db304996bf2e625fd9cf8
Haoran-Bao <albert_bao@hotmail.com>
2024-06-07 14:08:43 -0400

    add trainer

--- src/client/trainer.py
+++ src/client/trainer.py
@@ -22,3 +22,55 @@ from peft import (
     set_peft_model_state_dict,
     
 )
+
+tokenizer = LlamaTokenizerFast.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+
+class ModifiedTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        return model(
+            input_ids=inputs["input_ids"],
+            labels=inputs["labels"],
+        ).loss
+
+    def prediction_step(self, model: torch.nn.Module, inputs, prediction_loss_only: bool, ignore_keys = None):
+        with torch.no_grad():
+            res = model(
+                input_ids=inputs["input_ids"].to(model.device),
+                labels=inputs["labels"].to(model.device),
+            ).loss
+        return (res, None, None)
+
+    def save_model(self, output_dir=None, _internal_call=False):
+        from transformers.trainer import TRAINING_ARGS_NAME
+
+        os.makedirs(output_dir, exist_ok=True)
+        torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
+        saved_params = {
+            k: v.to("cpu") for k, v in self.model.named_parameters() if v.requires_grad
+        }
+        torch.save(saved_params, os.path.join(output_dir, "adapter_model.bin"))
+
+def data_collator(features: list) -> dict:
+    
+    len_ids = [len(feature["input_ids"]) for feature in features]
+    longest = max(len_ids)
+    input_ids = []
+    labels_list = []
+    for ids_l, feature in sorted(zip(len_ids, features), key=lambda x: -x[0]):
+        ids = feature["input_ids"]
+        pdb.set_trace()
+        seq_len = feature["seq_len"]
+        labels = (
+            [tokenizer.pad_token_id] * (seq_len - 1) + ids[(seq_len - 1) :] + [tokenizer.pad_token_id] * (longest - ids_l)
+        )
+        ids = ids + [tokenizer.pad_token_id] * (longest - ids_l)
+        _ids = torch.LongTensor(ids)
+        labels_list.append(torch.LongTensor(labels))
+        input_ids.append(_ids)
+    input_ids = torch.stack(input_ids)
+    labels = torch.stack(labels_list)
+    return {
+        "input_ids": input_ids,
+        "labels": labels,
+    }
