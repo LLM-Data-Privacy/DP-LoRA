55952210164fe2b5764f65c3aac99ed3fe7751ac
rgtdthrd <beik@rpi.edu>
2024-06-14 23:03:32 -0400

    wait to be tested

--- experiments/40_benchmark.py
+++ experiments/40_benchmark.py
@@ -1,72 +1,66 @@
-from sklearn.metrics import f1_score
 import transformers
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 from huggingface_hub import login
-import time
+from datasets import load_dataset
+from sklearn.metrics import f1_score
+import torch, time
+
+
+def compute_f1(TP,FP,FN):
+    precision = TP/(TP+FP)
+    recall = TP/(TP+FN)
+    f1 = 2*precision*recall/(precision+recall)
+    return f1
+
+def compute_prediction(predictions,references):
+    TP = 0
+    FP = 0
+    FN = 0
+    for i in range(len(predictions)):
+        pred = predictions[i]
+        ref = references[i]
+        for j in range(len(pred)):
+            if pred[j] == 1 and ref[j] == 1:
+                TP += 1
+            elif pred[j] == 1 and ref[j] == 0:
+                FP += 1
+            elif pred[j] == 0 and ref[j] == 1:
+                FN += 1
+    return TP,FP,FN
+
 TOKEN = "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"
 login(token = TOKEN)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-def preprocess_function(examples):
-    combined_texts = [q + " [SEP] " + t for q, t in zip(examples['query'], examples['text'])]
-    return tokenizer(combined_texts, truncation=True, padding="max_length", max_length=512)
-def compute_f1(predictions, references):
-    f1 = f1_score(references, predictions, average='weighted')
-    return f1
+print("Using device:", device)
 
-def collate_fn(batch):
-    input_ids = [item['input_ids'] for item in batch]
-    attention_masks = [item['attention_mask'] for item in batch]
-    
-    input_ids = [torch.tensor(ids) for ids in input_ids]
-    attention_masks = [torch.tensor(mask) for mask in attention_masks]
-    
-    input_ids_padded = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True, padding_value=tokenizer.pad_token_id)
-    attention_masks_padded = torch.nn.utils.rnn.pad_sequence(attention_masks, batch_first=True, padding_value=0)
-    
-    return {
-        'input_ids': input_ids_padded,
-        'attention_mask': attention_masks_padded
-    }
-
-
+# Load the model and tokenizer
 model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2",token = TOKEN).to(device) 
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2",token = TOKEN)
-
-if tokenizer.pad_token is None:
-    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-    model.resize_token_embeddings(len(tokenizer)) 
-
-    from datasets import load_dataset
-
-# If the dataset is gated/private, make sure you have run huggingface-cli login
 dataset = load_dataset("TheFinAI/flare-cd",token = TOKEN)
-processed_dataset = dataset.map(preprocess_function, batched=True)
-input_ids = processed_dataset['test']['input_ids']
-attention_mask = processed_dataset['test']['attention_mask']
-
-
-inputs = torch.tensor(input_ids)
-masks = torch.tensor(attention_mask)
-data = [{'input_ids': ids, 'attention_mask': mask} for ids, mask in zip(input_ids, attention_mask)]
-loader = torch.utils.data.DataLoader(data, batch_size=10, collate_fn=collate_fn, shuffle=False)
+dataset_iter = dataset['test'].iter(1)
 
 
 start_time = time.time()
-for batch in loader:
-    inputs = batch['input_ids']
-    masks = batch['attention_mask']
-    with torch.no_grad():
-        outputs = model(inputs, attention_mask=masks)
-        predictions = torch.argmax(outputs.logits, dim=-1)
-    print(predictions)
-    
+# Prepare the message
+message = [{"role": "user", "content": ""}]
+while True:
+    try:
+        data = next(dataset_iter)
+        query = data['query'][0]
+        message[0]["content"] = query
+
+        
+        encodeds = tokenizer.apply_chat_template(message, return_tensors="pt")
+        model_inputs = encodeds.to(device)
+        model.to(device)
+        generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
+        decoded = tokenizer.batch_decode(generated_ids)
+        print(decoded[0])
+    except StopIteration:
+        break
+
 end_time = time.time()
+
 elapsed_time = end_time - start_time
-
-print(f"Elapsed time for inference: {elapsed_time} seconds")
-
-actual_labels = dataset['train']['gold'] 
-f1 = compute_f1(predictions.numpy(), actual_labels)
-
-print(f"F1 Score: {f1}")
+print("Elapsed time: ", elapsed_time)
