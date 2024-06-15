d09d5cd508916ec7feb4c909e4be3e201b0563d8
rgtdthrd <beik@rpi.edu>
2024-06-14 23:37:23 -0400

    script to upload files to server

---
+++ experiments/server_script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -x
+srun python 4o_benchmark.py > benchmark_CD_result.txt
--- experiments/to_server.sh
+++ experiments/to_server.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-local_file="experiments\4o_benchmark.py"
+local_file="experiments/4o_benchmark.py"
+script_file="experiments/server_script.sh"
 username="FNAIkqbe"
 remote_host="blp04.ccni.rpi.edu"
 remote_dir="kaiqi_bei"
 
 
 scp "$local_file" ${username}@${remote_host}:${remote_dir}
+scp "$script_file" ${username}@${remote_host}:${remote_dir}
