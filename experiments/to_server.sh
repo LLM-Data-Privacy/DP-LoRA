4638e6e68a9aaf916027b4df347b50388b187a40
rgtdthrd <beik@rpi.edu>
2024-06-14 23:19:22 -0400

    sh to server

---
+++ experiments/to_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+local_file="experiments\4o_benchmark.py"
+username="FNAIkqbe"
+remote_host="blp04.ccni.rpi.edu"
+remote_dir="kaiqi_bei"
+
+
+scp "$local_file" ${username}@${remote_host}:${remote_dir}
