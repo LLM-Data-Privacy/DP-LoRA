bc8b120763a518ece3b77a3a94e83a3941edb161
dsarca02 <dsarca02@gmail.com>
2024-06-28 23:47:46 -0400

    added imports

--- mistral/benchmark_mistral_FinQA.py
+++ mistral/benchmark_mistral_FinQA.py
@@ -1,1 +1,10 @@
 #benchmark mistral-7b on FinQA dataset
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
+from datasets import Dataset
+import torch
+import os
+from dotenv import load_dotenv
