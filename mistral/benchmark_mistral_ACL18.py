2f977e65f879aee865ffd6c90a501d43d6226ecd
dsarca02 <dsarca02@gmail.com>
2024-07-30 13:47:40 -0400

    set up env variables

--- mistral/benchmark_mistral_ACL18.py
+++ mistral/benchmark_mistral_ACL18.py
@@ -1,10 +1,25 @@
 #benchmark mistral-7b on ACL18 dataset
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
-from datasets import Dataset
-import torch
 import os
+import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, logging
+from datasets import load_dataset, Dataset
+import torch
 from dotenv import load_dotenv
+from huggingface_hub import login
+import re
+
+# Set up environment variables for caching
+os.environ['TRANSFORMERS_CACHE'] = '/gpfs/u/home/FNAI/FNAIdosa/scratch/huggingface'
+os.environ['HF_DATASETS_CACHE'] = '/gpfs/u/home/FNAI/FNAIdosa/scratch/huggingface'
+os.environ['HF_HOME'] = '/gpfs/u/home/FNAI/FNAIdosa/scratch/huggingface'
+
+# Hugging Face setup
+load_dotenv()
+api_token = os.getenv("HUGGINGFACE_API_TOKEN")
+login(api_token)
+
+# Ensure directories exist
+os.makedirs('/gpfs/u/home/FNAI/FNAIdosa/scratch/huggingface', exist_ok=True)
+
