97af1f67bc08fd845d38b3f89d9bf919f87241fb
dsarca02 <dsarca02@gmail.com>
2024-08-13 22:52:26 -0400

    added the hugging face set up and loaded the dataset from hugging face

--- GPT4o/benchmark_4o_ma.py
+++ GPT4o/benchmark_4o_ma.py
@@ -8,3 +8,16 @@ from dotenv import load_dotenv
 from huggingface_hub import login
 import re
 import openai
+
+# Hugging Face setup
+load_dotenv()
+api_token = os.getenv("HUGGINGFACE_API_TOKEN")
+login(api_token)
+
+# Load the entire dataset
+try:
+    dataset = load_dataset("TheFinAI/flare-ma", split="test")
+    print(dataset)
+except Exception as e:
+    print(f"Error loading dataset: {e}")
+    exit(1)
