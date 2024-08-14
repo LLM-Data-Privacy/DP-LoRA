b56915064a4611460dac165acd7f22a3f6133325
dsarca02 <dsarca02@gmail.com>
2024-08-13 22:57:54 -0400

    aded the prep_prompt function and derive_answer

--- GPT4o/benchmark_4o_ma.py
+++ GPT4o/benchmark_4o_ma.py
@@ -21,3 +21,27 @@ try:
 except Exception as e:
     print(f"Error loading dataset: {e}")
     exit(1)
+
+# Preprocess prompt function
+def prep_prompt(row):
+    prompt = """In this task, you will be given Mergers and Acquisitions (M&A) news articles or tweets.\n 
+                Your task is to classify each article or tweet based on whether the mentioned deal was \n
+                completed or remained a rumour. Your response should be a single word - either 'complete'\n
+                or 'rumour' - representing the outcome of the deal mentioned in the provided text.\n
+                Enclose your output with the token [OTPT] and [CTPT]."""
+    profile = row['text']
+    answer = "\nAnswer: "
+    return prompt + profile + answer
+
+def derive_answer(output_raw):
+    match = re.search(r'\[OTPT\](.*?)\[CTPT\]', output_raw)
+    if match:
+        answer = match.group(1).strip().lower()
+        if answer in {"rumour", "complete"}:
+            return answer
+    output_raw = output_raw.strip().lower()
+    if "rumour" in output_raw:
+        return "rumour"
+    if "complete" in output_raw:
+        return "complete"
+    return None
